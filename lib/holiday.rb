9792adba0834840c69756c53a884b8f4c60ff65c
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-03-06 05:50:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -47,7 +47,7 @@ holiday_hash[season][holiday_name] = supply_array
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  # return an array of all of the supplies that are used in the winter season
+  holiday_hash[:winter].values# return an array of all of the supplies that are used in the winter season
 
 end
 
